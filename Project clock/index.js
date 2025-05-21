56ee8350fea633e86a68d7fdae65adc460604558
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-05-21 20:23:41 +0530

    Uploaded the clock project as a landing page

---
+++ Project clock/index.js
@@ -0,0 +1,21 @@
+setInterval(()=>{
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
+
+
+//dark theme
+const toggle = document.getElementById('sun');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('fa-moon');
+    document.body.classList.toggle('dark-theme');
+});
